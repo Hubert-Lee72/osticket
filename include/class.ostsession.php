0443ddb2499f9fc1148d5fc0696d0cede869d0f9
Hubert_CJLee <84006263+Hubert-Lee72@users.noreply.github.com>
2021-07-23 16:21:46 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -184,8 +184,6 @@ extends SessionBackend {
             ])->one();
             $this->id = $id;
         }
-        $this->data->session_data = "";
-        }
         catch (DoesNotExist $e) {
             $this->data = new SessionData(['session_id' => $id]);
         }
